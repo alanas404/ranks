2cc69d67a420e7357c5082a0fa4bc8a3be26d7d4
alanas404 <al.anas404040@gmail.com>
2024-12-17 00:14:36 +0600

    update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+"use client";
 import Image from "next/image";
 import Description from "./components/Description";
 import ReviewsSection from "./components/ReviewsSection";
@@ -8,17 +9,19 @@ import OrderDetails from "./components/OrderDetails";
 import PaymentDetails from "./components/PaymentDetails";
 import OrderItems from "./components/OrderItems";
 import GiftOrderItems from "./components/GiftOrderItems";
+import ModalComponent from "./components/ModalComponent";
 
 export default function Home() {
   return (
     <div className="container  mx-auto px-4 md:p-0">
-       {/* <Order/>
+       <Order/>
        <LoyaltyPoint/> 
        <LoyaltyPointCard/> 
        <OrderDetails/>
        <PaymentDetails/>
-       <OrderItems/> */}
+       <OrderItems/>
        <GiftOrderItems/>
+       <ModalComponent/>
     </div>
   );
 }
